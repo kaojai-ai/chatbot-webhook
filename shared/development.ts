9eeae4033d40c501c061d9c55676bce459ad1608
chaintng <chaintng@gmail.com>
2025-09-13 19:13:44 +0700

    chatbot webhook

--- shared/development.ts
+++ shared/development.ts
@@ -1,3 +1,3 @@
 export const isDev = ["local", "dev"].includes(process.env.NODE_ENV || "");
 
-export const serviceName = process.env.OTEL_SERVICE_NAME || 'kj-checkslip-bot';
+export const serviceName = process.env.OTEL_SERVICE_NAME || 'kj-chatbot-webhook';
